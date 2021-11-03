8e4d45458d91c25412b08f30ea1956cb14620313
Serge Leger <Serge.Leger@nrc-cnrc.gc.ca>
2021-11-03 14:17:31 -0300

    Squashed commit of the following:
    commit 0adabb7528af01c245686bf31d768f0b1cf43b67
    Author: Serge Leger <Serge.Leger@nrc-cnrc.gc.ca>
    Date:   Wed Nov 3 14:17:08 2021 -0300
    
        Corrected Yearly component (missing August)
    
    commit 012b4ccd181e2498bb4a79828d0e11b00029889f
    Author: Serge Leger <Serge.Leger@nrc-cnrc.gc.ca>
    Date:   Wed Nov 3 13:56:35 2021 -0300
    
        Checking yarn package management.
    
    commit 1bf12b1cfedc11ab73df8f32532ab8dc5951e22e
    Author: Serge Leger <Serge.Leger@nrc-cnrc.gc.ca>
    Date:   Wed Nov 3 12:26:33 2021 -0300
    
        Bumped Yarn to version 3.1.0

--- SvelteUI/rollup.config.js
+++ SvelteUI/rollup.config.js
@@ -1,16 +1,16 @@
-import svelte from 'rollup-plugin-svelte';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';
-import css from 'rollup-plugin-css-only';
+import svelte from "rollup-plugin-svelte";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
+import css from "rollup-plugin-css-only";
 import replace from "@rollup/plugin-replace";
 
 const production = !process.env.ROLLUP_WATCH;
 
-let serviceURL = "http://pi4:5000/api";
+let serviceURL = "http://192.168.2.16:5000/api";
 if (production) {
 	serviceURL = "/api";
 }
@@ -25,36 +25,36 @@ function serve() {
 	return {
 		writeBundle() {
 			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
+			server = require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+				stdio: ["ignore", "inherit", "inherit"],
+				shell: true,
 			});
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
+			process.on("SIGTERM", toExit);
+			process.on("exit", toExit);
+		},
 	};
 }
 
 export default {
-	input: 'src/main.ts',
+	input: "src/main.ts",
 	output: {
 		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
+		format: "iife",
+		name: "app",
+		file: "public/build/bundle.js",
 	},
 	plugins: [
 		svelte({
 			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: {
 				// enable run-time checks when not in production
-				dev: !production
-			}
+				dev: !production,
+			},
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+		css({ output: "bundle.css" }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
@@ -63,17 +63,17 @@ export default {
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: ['svelte']
+			dedupe: ["svelte"],
 		}),
 		commonjs(),
 		typescript({
 			sourceMap: !production,
-			inlineSources: !production
+			inlineSources: !production,
 		}),
 
 		replace({
-			'SERVICE_URL': JSON.stringify(serviceURL),
-			'preventAssignment': true
+			SERVICE_URL: JSON.stringify(serviceURL),
+			preventAssignment: true,
 		}),
 
 		// In dev mode, call `npm run start` once
@@ -82,13 +82,13 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload("public"),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
 	],
 	watch: {
-		clearScreen: false
-	}
+		clearScreen: false,
+	},
 };
